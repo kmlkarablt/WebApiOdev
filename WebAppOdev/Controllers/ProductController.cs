6d146f7719fbbcd102fe06b17b548dd21b5657a4
Kemal <kmlkarablt@gmail.com>
2024-08-05 12:21:06 +0300

    WebApiOdev

---
+++ WebAppOdev/Controllers/ProductController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Category { get; set; }
+}
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private static List<Product> _products = new List<Product>();
+    private static int _nextId = 1;
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Product>> GetProducts()
+    {
+        return Ok(_products);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Product> GetProduct(int id)
+    {
+        var product = _products.FirstOrDefault(p => p.Id == id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        return Ok(product);
+    }
+
+    [HttpPost]
+    public ActionResult<Product> CreateProduct(Product product)
+    {
+        product.Id = _nextId++;
+        _products.Add(product);
+        return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateProduct(int id, Product product)
+    {
+        var existingProduct = _products.FirstOrDefault(p => p.Id == id);
+        if (existingProduct == null)
+        {
+            return NotFound();
+        }
+
+        existingProduct.Name = product.Name;
+        existingProduct.Price = product.Price;
+        existingProduct.Category = product.Category;
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteProduct(int id)
+    {
+        var product = _products.FirstOrDefault(p => p.Id == id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        _products.Remove(product);
+        return NoContent();
+    }
+}
